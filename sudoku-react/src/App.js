b91df4b7d02e78d00d17791a14fea09131dff309
Jeffrey Parsons <jeffparsons_312@hotmail.com>
2020-05-14 07:29:47 -0500

    initial commit

---
+++ sudoku-react/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <h1>This is just a test react app. I really have no idea what im doing.</h1>
+  );
+}
+
+export default App;
